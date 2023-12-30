14cbb46e15a6eb567fff002de9c04a5009ed5d8d
selimnill <mrrafi812345@gmail.com>
2023-12-30 21:23:14 +0600

    modified

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,9 +2,17 @@ import "./Cart.scss";
 import { MdClose } from "react-icons/md";
 import { BsCartX } from "react-icons/bs";
 import CartItem from "./CartItem/CartItem";
+import { useContext } from "react";
+import { Context } from "../../utils/context";
+import { useNavigate } from "react-router-dom";
 
 
 const Cart = ({ setShowCart }) => {
+
+    const {cartItems, cartSubTotal} = useContext(Context);
+
+    const navigate = useNavigate();
+
     return <div className="cart-panel">
         <div className="cart-content">
             <div className="cart-header">
@@ -14,24 +22,24 @@ const Cart = ({ setShowCart }) => {
                     <span className="text">Close</span>
                 </span>
             </div>
-            {/* <div className="empty-cart">
+            {!cartItems.length && <div className="empty-cart">
                 <BsCartX />
                 <span>No producs in the Cart</span>
-                <button className="return-cta">RETURN TO SHOP</button>
-            </div> */}
+                <button className="return-cta" onClick={() => navigate("/")}>RETURN TO SHOP</button>
+            </div> }
 
-            <>
+           {!!cartItems.length && <>
                 <CartItem />
                 <div className="cart-footer">
                     <div className="subtotal">
                         <span className="text">Subtotal: </span>
-                        <span className="total">$12345</span>
+                        <span className="total">&#36; {cartSubTotal}</span>
                     </div>
                     <div className="button">
                         <button className="checkout-cta">CheckOut</button>
                     </div>
                 </div>
-            </>
+            </>}
 
         </div>
     </div>;
